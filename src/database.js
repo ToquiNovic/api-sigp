fa505e37f1d0d625e6cfa8ff2906c5fe9418caa6
ToquiNovic <j.toquica@udla.edu.co>
2023-03-23 10:24:17 -0500

    Pool-mysql2

--- src/database.js
+++ src/database.js
@@ -1,14 +1,17 @@
 const mysql = require("mysql2");
 const { DB_HOST, DB_NAME, DB_PASS, DB_USER } = require("./config");
 
-const mysqlConnection = mysql.createConnection({
+const mysqlConnection = mysql.createPool({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASS,
   database: DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-mysqlConnection.connect(function (err) {
+mysqlConnection.getConnection(function (err) {
   if (err) {
     console.log(err);
     return;
