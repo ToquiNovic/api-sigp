6d1a8deb20a25e3471d482b1caef00f54a05875a
DairoGN <50602257+Dairo01001@users.noreply.github.com>
2023-05-24 01:04:55 -0500

    update consult

--- src/routes/elements.js
+++ src/routes/elements.js
@@ -5,20 +5,71 @@ elementsRouter.get("/", (req, res) => {
   const { limit } = req.query;
 
   let mysqlQuery = `
-    SELECT ELEMENTOS.ELEM_IDELEMENTO AS id, ELEMENTOS.ELEM_NOMBREELEMENTO AS name, ELEMENTOS.ELEM_ESTADO AS state, ELEMENTOS.ELEM_MARCA AS brand
-    FROM ELEMENTOS
-    ORDER BY ELEMENTOS.ELEM_IDELEMENTO ASC
+    SELECT
+      ELEMENTOS.ELEM_IDELEMENTO AS id,
+      ELEMENTOS.ELEM_NOMBREELEMENTO AS name,
+      ELEMENTOS.ELEM_ESTADO AS state, 
+      ELEMENTOS.ELEM_MARCA AS brand, 
+      CANTIDAD.CANT_NOMBRECATEGORIA as category,
+      COUNT(*) AS stock
+    FROM 
+      ELEMENTOS
+    INNER JOIN 
+      CANTIDAD
+    ON 
+      ELEMENTOS.FK_CANT_IDCANTIDAD = CANTIDAD.CANT_IDCANTIDAD
+    WHERE 
+      ELEMENTOS.ELEM_ESTADO = 'Disponible'
+    GROUP BY 
+      ELEMENTOS.ELEM_NOMBREELEMENTO,
+      ELEMENTOS.ELEM_ESTADO
+    ORDER BY 
+      ELEMENTOS.ELEM_IDELEMENTO ASC
   `;
 
-  if(limit) {
-    mysqlQuery = mysqlQuery + `LIMIT ${limit};`
+  if (limit) {
+    mysqlQuery = mysqlQuery + `LIMIT ${limit};`;
   } else {
-    mysqlQuery = mysqlQuery + ";"
+    mysqlQuery = mysqlQuery + ";";
   }
 
   mysql.query(mysqlQuery, (err, rows) => {
+    console.log(err);
     if (!err) {
-      return res.json(rows);
+      res.json(rows);
+    } else {
+      res.status(500).json({ msg: "Error!" });
+    }
+  });
+});
+
+elementsRouter.get("/:id", (req, res) => {
+  const { id } = req.params;
+  let mysqlQuery = `
+    SELECT
+      ELEMENTOS.ELEM_NOMBREELEMENTO AS name, 
+      COUNT(ELEMENTOS.ELEM_NOMBREELEMENTO) AS stock,
+      ELEMENTOS.ELEM_IDELEMENTO AS id, 
+      ELEMENTOS.ELEM_ESTADO AS state, 
+      ELEMENTOS.ELEM_MARCA AS brand, 
+      CANTIDAD.CANT_NOMBRECATEGORIA as category
+    FROM 
+      ELEMENTOS
+    INNER 
+      JOIN CANTIDAD
+    ON 
+      ELEMENTOS.FK_CANT_IDCANTIDAD = CANTIDAD.CANT_IDCANTIDAD
+    WHERE 
+      ELEMENTOS.ELEM_IDELEMENTO = ${id};
+  `;
+
+  mysql.query(mysqlQuery, (err, rows) => {
+    if (!err) {
+      if (rows[0]) {
+        res.json(rows[0]);
+      } else {
+        res.status(404).json({ msg: "Elemento no encontrado" });
+      }
     } else {
       res.status(500).json({ msg: "Error!" });
     }
